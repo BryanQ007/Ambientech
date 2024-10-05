76d1fe273056d94c3e6938d177ca0cfd84690861
MartinFLR <132178630+MartinFLR@users.noreply.github.com>
2024-10-05 18:40:52 -0300

    Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,66 +4,50 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import * as L from 'leaflet';
 import { ModalComponent } from './modal/modal.component';
-
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ModalFormularioComponent } from './modalFormulario/modalFormulario.component';
+import { MapaService } from './mapa.service';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule,ModalComponent],
+  imports: [RouterOutlet,CommonModule,ModalComponent, MatDialogModule, ModalFormularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   map: L.Map | undefined;
   lat = -45.8749;
   lng = -67.5203;
+  private markers: L.Marker[] = [];
+  componentToShow: string | null = null;
 
-  constructor() { }
+  constructor(public mapaService: MapaService) { }
 
   ngOnInit() {
-    this.initMap();
+    this.mapaService.initMap('map');
   }
 
-  initMap() {
-    this.map = L.map('map').setView([this.lat,this.lng], 13
 
-    );
-
-    // Capa base de OpenStreetMap
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap'
-    }).addTo(this.map);
-
-    // Capa WMTS de GIBS para MODIS Terra Aerosol
-    const gibsLayer = L.tileLayer('https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/MODIS_Terra_Aerosol/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png', {
-      maxZoom: 8, // Ajusta según la resolución que necesites
-      attribution: 'NASA GIBS',
-    });
-
-    // Añade la capa de GIBS al mapa
-    gibsLayer.addTo(this.map);
-  
-
- // Escuchar el evento de clic en el mapa
- this.map.on('click', this.onMapClick.bind(this));  // Usamos .bind(this) para mantener el contexto adecuado
-}
 
 // Método que se ejecuta al hacer clic en el mapa
 onMapClick(e: any): void {
   const coords = e.latlng;
-  const lat = coords.lat;
-  const lng = coords.lng;
 
-  // Mostrar las coordenadas en consola (opcional)
-  console.log(`Coordenadas: ${lat}, ${lng}`);
+  if (this.mapaService.existMarker(coords)) {
+    // Si el marcador existe, no abrir el modal
+    this.mapaService.vistaCrear = false;
+    this.mapaService.vistaVer = true;
+    console.log('El marcador ya existe en estas coordenadas.');
+  } else {
+    // Si no existe, abrir el modal
+    this.mapaService.vistaCrear = true;
+    this.mapaService.vistaVer = false;
 
-  // Crear un marcador en las coordenadas del clic
-  const marker = L.marker([lat, lng]);
-
-  // Añadir el marcador al mapa y mostrar un popup con las coordenadas
-  marker.addTo(this.map!).bindPopup(`Coordenadas: ${lat}, ${lng}`).openPopup();
+    // Agregar el marcador al array
+    this.mapaService.crearMarker(coords);
+  }
 }
-//funca
 }
---
+++ src/app/mapa.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import * as L from 'leaflet';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapaService {
+  map: L.Map | undefined;
+  lat = -45.8749;
+  lng = -67.5203;
+  markers: L.Marker[] = [];
+  vistaVer: boolean = false;
+  vistaCrear: boolean = false;
+
+  constructor() { }
+
+  initMap(mapElementId: string) {
+    this.map = L.map(mapElementId).setView([this.lat, this.lng], 13);
+
+    // Capa base de OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(this.map);
+
+    // Escuchar el evento de clic en el mapa
+    this.map.on('click', this.onMapClick.bind(this));
+  }
+
+  // Método que se ejecuta al hacer clic en el mapa
+  onMapClick(e: any): void {
+    const coords = e.latlng;
+
+    if (this.existMarker(coords)) {
+      // Si el marcador existe, no abrir el modal
+      this.vistaCrear = false;
+      this.vistaVer = true;
+      console.log('El marcador ya existe en estas coordenadas.');
+    } else {
+      // Si no existe, abrir el modal
+      this.vistaCrear = true;
+      this.vistaVer = false;
+      e = false;
+      // Crear el marcador
+      this.crearMarker(coords);
+    }
+  }
+
+  existMarker(coordenada: any): boolean {
+    return this.markers.some(marker => marker.getLatLng().equals(coordenada));
+  }
+
+  crearMarker(coords: any) {
+    const marker = L.marker([coords.lat, coords.lng]).addTo(this.map!);
+    marker.bindPopup(`Coordenadas: ${coords.lat}, ${coords.lng}`).openPopup();
+    this.markers.push(marker);
+  }
+}
