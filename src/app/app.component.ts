dbae4914d874c075fdff1bb0c0154e69f6b4e7f6
MartinFLR <132178630+MartinFLR@users.noreply.github.com>
2024-10-05 12:44:17 -0300

    hay recuadros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { EarthdataService } from './earthdata.service';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-root',
@@ -11,27 +13,30 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngSpace';
+  map: L.Map | undefined;
 
-  imageUrl: string|undefined;
-
-  data: any;
-
-  constructor(private earthData: EarthdataService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.earthData.getBestAvailable().subscribe({
-      next: response => {
-        const objectUrl = URL.createObjectURL(response);
-        this.imageUrl = objectUrl;
-        console.log('Imagen recibida:', this.imageUrl);
-      },
-      error: error => {
-        console.error('Error fetching data from GIBS:', error);
-      },
-      complete: () => {
-        console.log('Solicitud completada.');
-      }
+    this.initMap();
+  }
+
+  initMap() {
+    this.map = L.map('map').setView([0, 0], 2);
+
+    // Capa base de OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(this.map);
+
+    // Capa WMTS de GIBS
+    const gibsLayer = L.tileLayer('https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/{z}/{y}/{x}.png', {
+      maxZoom: 8, // Ajusta según la resolución que necesites
+      attribution: 'NASA GIBS',
     });
+
+    // Añade la capa de GIBS al mapa
+    gibsLayer.addTo(this.map);
   }
 }
