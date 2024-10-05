22b1404aaa54ff1a7b3172b3775378269437d171
MartinFLR <132178630+MartinFLR@users.noreply.github.com>
2024-10-05 12:19:56 -0300

    funciona poquito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { EarthdataService } from './earthdata.service';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ngSpace';
+
+  imageUrl: string|undefined;
+
+  data: any;
+
+  constructor(private earthData: EarthdataService) { }
+
+  ngOnInit() {
+    this.earthData.getBestAvailable().subscribe({
+      next: response => {
+        const objectUrl = URL.createObjectURL(response);
+        this.imageUrl = objectUrl;
+        console.log('Imagen recibida:', this.imageUrl);
+      },
+      error: error => {
+        console.error('Error fetching data from GIBS:', error);
+      },
+      complete: () => {
+        console.log('Solicitud completada.');
+      }
+    });
+  }
 }
--- src/app/earthdata.service.ts
+++ src/app/earthdata.service.ts
@@ -1,10 +1,32 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, NgModule } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EarthdataService {
 
-constructor() { }
+
+private baseUrl = 'https://gibs.earthdata.nasa.gov/wmts/epsg4326/'
+
+constructor(private http: HttpClient) { }
+
+
+getBestAvailable(): Observable<any> {
+  return this.http.get(`${this.baseUrl}best/`, { responseType: 'blob' });
+}
+
+getStandard(): Observable<any> {
+  return this.http.get(`${this.baseUrl}std/`);
+}
+
+getNearRealTime(): Observable<any> {
+  return this.http.get(`${this.baseUrl}nrt/`);
+}
+
+getAll(): Observable<any> {
+  return this.http.get(`${this.baseUrl}all/`);
+}
 
 }
